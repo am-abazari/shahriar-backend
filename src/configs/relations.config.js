bdc3b1cdda710b4386ff5d88158f3924075a6d62
Amirhossein <amirhossein.abazari@yahoo.com>
2026-04-16 11:54:49 +0330

    [ADD] auth model

--- src/configs/relations.config.js
+++ src/configs/relations.config.js
@@ -1,6 +1,7 @@
 const { CoupletModel } = require("@model/couplet.model");
 const { PoemModel } = require("@model/poem.model");
 const { UserModel } = require("@model/user.model");
+const { AuthModel } = require("../model/auth.model");
 
 const CreateRelations = () => {
   PoemModel.hasMany(CoupletModel, {
@@ -14,6 +15,14 @@ const CreateRelations = () => {
   UserModel.hasMany(PoemModel, {
     foreignKey: {
       name: "writerID",
+    },
+    sourceKey: "id",
+  });
+
+  UserModel.hasOne(AuthModel, {
+    foreignKey: {
+      name: "userID",
+      onDelete: "CASCADE",
     },
     sourceKey: "id",
   });
